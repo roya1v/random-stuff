35f82478d10b06bf67930f2af5cf1680abdebeec
Mike JR <mikedalvik@ya.ru>
2023-03-02 21:59:20 +0100

    Created some to do app!

--- rusty-to-do/src/main.rs
+++ rusty-to-do/src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let command = args.get(1);
+    match command {
+        None => show_all(),
+        Some(command) => match command.as_str() {
+            "add" => add_todo(args[2].as_str()),
+            _ => println!("Unknown command!")
+        }
+    }
+}
+
+fn show_all() {
+    let connection = sqlite::open("rusty.db").unwrap();
+    let query = "SELECT * FROM todos";
+    println!("TODOs:");
+    connection
+    .iterate(query, |pairs| {
+        for &(_, value) in pairs.iter() {
+            println!("- {}", value.unwrap());
+        }
+        true
+    })
+    .unwrap();
+}
+
+fn add_todo(text: &str) {
+    let connection = sqlite::open("rusty.db").unwrap();
+    let query = format!("
+    CREATE TABLE IF NOT EXISTS todos (text TEXT);
+    INSERT INTO todos VALUES ('{text}');
+    ");
+    connection.execute(query).unwrap();
 }
