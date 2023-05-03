6f9f0d807de8882dbda3ab7539dddfe95b37bc47
Mike <m.shevelinski@gmail.com>
2023-05-03 21:26:12 +0200

    And now a window

--- trying_sdl/main.c
+++ trying_sdl/main.c
@@ -3,9 +3,39 @@
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-int main() {
-      SDL_ShowSimpleMessageBox( SDL_MESSAGEBOX_INFORMATION, 
-              "Hello World",
-              "You have successfully compiled and linked an SDL2"
-              " program, congratulations.", NULL );
+int main()
+{
+    SDL_Window *win = NULL;
+    SDL_Renderer *ren = NULL;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_CreateWindowAndRenderer(320, 640, 0, &win, &ren);
+
+    SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+    SDL_RenderClear(ren);
+    SDL_RenderPresent(ren);
+    int quit = 0;
+
+    // Event handler
+    SDL_Event e;
+
+    // While application is running
+    while (!quit)
+    {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0) // poll for event
+        {
+            // User requests quit
+            if (e.type == SDL_QUIT) // unless player manually quits
+            {
+                quit = 1;
+            }
+        }
+    }
+
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+    return (0);
 }
