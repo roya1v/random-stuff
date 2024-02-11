e3510ae6190cffedc46dd1cb496b4f7c73c713f3
Mike <m.shevelinski@gmail.com>
2024-02-11 21:54:04 +0100

    Convert AppDelegate and SceneDelegate to objc

---
+++ to-do-collection/objc-ios-app/BetterToDo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BetterToDo
+//
+//  Created by Mike S. on 11/02/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ to-do-collection/objc-ios-app/BetterToDo/SceneDelegate.h
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.h
+//  BetterToDo
+//
+//  Created by Mike S. on 11/02/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
