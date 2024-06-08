5b6b7eb139d6496ce9b45ff8a9c1ca7ccf8ede29
Mike <m.shevelinski@gmail.com>
2024-06-08 14:55:49 +0200

    Create new project

---
+++ bicycle-app/BicycleApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BicycleApp
+//
+//  Created by Mike S. on 08/06/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
