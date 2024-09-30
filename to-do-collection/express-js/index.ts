639368b3f055255ea300616113d83ec2b22c1d04
Mike <m.shevelinski@gmail.com>
2024-09-30 20:51:26 +0200

    Implement almost v1 api

--- to-do-collection/express-js/index.ts
+++ to-do-collection/express-js/index.ts
@@ -1,39 +1,59 @@
-import express from 'express';
+import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
 
 const app = express();
 const port = 8000;
 
+const mongoUrl = process.env.MONGO_URL!;
+const client = new MongoClient(mongoUrl);
+const database = client.db("todos_db");
+const collection = database.collection<ToDoItem>("todos");
+
+interface ToDoItem {
+  _id: ObjectId | null;
+  content: String;
+  is_done: Boolean;
+}
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get('/todos', (req, res) => {
-  res.send([
-    {id: 1, content: "Test",  is_done: false}
-  ]);
+app.get("/todos", async (req, res) => {
+  const items = await collection.find({}).toArray();
+  res.json(items);
 });
 
-app.post('/todos', (req, res) => {
-    res.send({
-        id: 1,
-        content: "Test",
-        is_done: false
-    });
+app.post("/todos", async (req, res) => {
+  const item: ToDoItem = {
+    _id: null,
+    content: req.body.content,
+    is_done: req.body.is_done,
+  };
+  const newItem = await collection.insertOne(item);
+  res.json({
+    _id: newItem.insertedId,
+    content: req.body.content,
+    is_done: req.body.is_done,
+  });
 });
 
-app.put('/todos', (req, res) => {
-    res.send({
-        id: 1,
-        content: "Test",
-        is_done: false
-    });
+app.put("/todos", async (req, res) => {
+  const item: ToDoItem = {
+    _id: req.body._id,
+    content: req.body.content,
+    is_done: req.body.is_done,
+  };
+  const result = await collection.updateOne({ _id: item._id }, { $set: item });
+  console.log(result);
+  res.status(200).json(item);
 });
 
-app.delete('/todos/:todoId/delete', (req, res) => {
-    res.send({
-        id: 1,
-        content: "Test",
-        is_done: false
-    });
+app.delete("/todos/:todoId/delete", async (req, res) => {
+  const id = req.params.todoId;
+
+  await collection.deleteOne({ _id: new ObjectId(id) });
+  res.status(200).send(`Successfully deleted item with id ${id}`);
 });
