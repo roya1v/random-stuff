9864515db8af5849a48aa6cc31d4f9e0a4426a5b
Mike <m.shevelinski@gmail.com>
2025-01-01 12:42:54 +0100

    Create android app

---
+++ swift-multiplatform/android/app/src/main/java/com/example/dataloggingsdkapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.dataloggingsdkapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.dataloggingsdkapp.ui.theme.DataLoggingSDKAppTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DataLoggingSDKAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Content(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Content(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Button(onClick = {
+            print("Hello world")
+        }) {
+            Text("Start")
+        }
+        Button(onClick = {
+            print("Hello world")
+        }) {
+            Text("Stop")
+        }
+    }
+}
