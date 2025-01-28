c047721b2a4c18c36adc06bec2db585213653a81
Mike <m.shevelinski@gmail.com>
2025-01-28 21:42:07 +0100

    Form changes

--- json-schema-migrator/Sources/JsonSchemaMigrator.swift
+++ json-schema-migrator/Sources/JsonSchemaMigrator.swift
@@ -10,31 +10,101 @@ struct JsonSchemaMigrator {
     }
 
     static func compare(_ old: JsonSchema, _ new: JsonSchema) {
-        compareProperties(old.properties, new.properties)
+        let changes = compareProperties(old.properties, new.properties, path: "")
+
+        for change in changes {
+            switch change.kind {
+            case .deletedProperty(let string):
+                print("\(change.path): Deleted \(string)")
+            case .addedProperty(let string, let property):
+                print("\(change.path): Added property \(string) - \(property)")
+            case .deletedItem:
+                print("\(change.path): Deleted item")
+            case .addedItem(let property):
+                print("\(change.path): Added item - \(property)")
+            case .changeType(let jsonType):
+                print("\(change.path): Changed type - \(jsonType)")
+            }
+        }
     }
 
-    static func compareProperties(_ old: JsonSchema.Properties, _ new: JsonSchema.Properties) {
-        for (oldPropertyName, oldProperty) in old {
-            if let newSchemaProperty = new[oldPropertyName] {
-                if newSchemaProperty != oldProperty {
-                    if let oldItem = oldProperty.item, let newItem = newSchemaProperty.item, oldItem != newItem {
-                        compareProperties(oldItem.properties ?? [:],
-                                          newItem.properties ?? [:])
-                    } else {
-                        debugPrint("Property '\(oldPropertyName)' changed from \(oldProperty) to \(newSchemaProperty)")
+    static func compareProperties(_ old: JsonSchema.Properties, _ new: JsonSchema.Properties, path: String) -> [Change] {
+        guard old != new else {
+            return []
+        }
+        var result = [Change]()
+
+        for (propertyName, oldProperty) in old {
+            if let newProperty = new[propertyName] {
+                result.append(contentsOf: comprateProperty(oldProperty, newProperty, path: "\(path)/\(propertyName)"))
+            } else {
+                result.append(.init(path: path, kind: .deletedProperty(propertyName)))
+            }
+        }
+
+        for (propertyName, newProperty) in new {
+            if old[propertyName] == nil {
+                result.append(.init(path: path, kind: .addedProperty(propertyName, newProperty)))
+            }
+        }
+        return result
+    }
+
+    static func comprateProperty(_ old: JsonSchema.Property, _ new: JsonSchema.Property, path: String) -> [Change] {
+        guard old != new else {
+            return []
+        }
+        var result = [Change]()
+
+        if old.type != new.type {
+            result.append(.init(path: path, kind: .changeType(new.type)))
+        }
+
+        if old.item != new.item {
+            if let newItem = new.item {
+                if let oldItem = old.item {
+                    result.append(contentsOf: comprateProperty(oldItem, newItem, path: path))
+                } else {
+                    result.append(.init(path: path, kind: .addedItem(newItem)))
+                }
+            } else if old.item != nil {
+                result.append(.init(path: path, kind: .deletedItem))
+            }
+        }
+
+        if old.properties != new.properties {
+            if let newProperties = new.properties {
+                if let oldProperties = old.properties {
+                    result.append(contentsOf: compareProperties(oldProperties, newProperties, path: path))
+                } else {
+                    for (propertyName, property) in newProperties {
+                        result.append(.init(path: path, kind: .addedProperty(propertyName, property)))
                     }
                 }
-            } else {
-                debugPrint("Property '\(oldPropertyName) deleted")
+            } else if let oldProperties = old.properties {
+                for (propertyName, _) in oldProperties {
+                    result.append(.init(path: path, kind: .deletedProperty(propertyName)))
+                }
             }
         }
 
-        for (newPropertyName, newProperty) in new {
-            if old[newPropertyName] == nil {
-                debugPrint("New property '\(newPropertyName)' is \(newProperty)")
-            }
-        }
+        return result
     }
+}
+
+struct Change {
+    enum Kind {
+        case deletedProperty(String)
+        case addedProperty(String, JsonSchema.Property)
+
+        case deletedItem
+        case addedItem(JsonSchema.Property)
+
+        case changeType(JsonSchema.Property.JsonType)
+    }
+
+    let path: String
+    let kind: Kind
 }
 
 struct JsonSchema: Codable {
