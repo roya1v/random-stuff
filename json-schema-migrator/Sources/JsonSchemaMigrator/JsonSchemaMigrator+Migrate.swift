38c50df5257a8031df5d3818d636581338cf8f0b
Mike <m.shevelinski@gmail.com>
2025-01-28 23:38:46 +0100

    Cover more cases

--- json-schema-migrator/Sources/JsonSchemaMigrator/JsonSchemaMigrator+Migrate.swift
+++ json-schema-migrator/Sources/JsonSchemaMigrator/JsonSchemaMigrator+Migrate.swift
@@ -29,29 +29,54 @@ extension Math {
                 apply(change, to: &json)
             }
 
-            print(json)
+//            print(json)
 
             let outputData = try JSONEncoder().encode(json)
             try outputData.write(to: URL(filePath: output))
         }
 
         private func apply(_ change: Change, to json: inout JsonValue) {
+            if let child = change.child,
+               let pathComponent = change.path.first {
+                print("/\(pathComponent)", terminator: "")
+                apply(child, to: &json[pathComponent]!)
+                return
+            }
+            print(": ", terminator: "")
             switch change.kind {
-            case .deletedProperty(let string):
-                if let child = change.child {
-                    apply(child, to: &json[change.path.first!]!)
-                }
-                json[string] = nil
-            case .addedProperty(let string, let property):
-                print("Provide the default value for \(string)")
-                let test = readLine()!
-                json[string] = .string(test)
+            case .deletedProperty(let propertyName):
+                json[propertyName] = nil
+                print("Deleted '\(propertyName)'")
+            case .addedProperty(let propertyName, let property):
+                addNewProperty(propertyName, property: property, json: &json)
             case .deletedItem:
                 fatalError()
             case .addedItem(let property):
                 fatalError()
             case .changeType(let jsonType):
                 fatalError()
+            }
+        }
+
+        private func addNewProperty(_ key: String, property: JsonSchema.Property, json: inout JsonValue) {
+            print("Provide the default \(property.type) value for \(key)")
+            guard let input = readLine() else {
+                addNewProperty(key, property: property, json: &json)
+                return
+            }
+            switch property.type {
+            case .string:
+                json[key] = .string(input)
+            case .object:
+                fatalError()
+            case .array:
+                fatalError()
+            case .integer:
+                guard let integer = Int(input) else {
+                    addNewProperty(key, property: property, json: &json)
+                    return
+                }
+                json[key] = .integer(integer)
             }
         }
     }
