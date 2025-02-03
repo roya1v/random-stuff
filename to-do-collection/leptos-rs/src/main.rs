3b7a091124789ba4b1e4e2958142414097b7c1e9
Mike <m.shevelinski@gmail.com>
2025-02-03 21:42:42 +0100

    Add tailwind

--- to-do-collection/leptos-rs/src/main.rs
+++ to-do-collection/leptos-rs/src/main.rs
@@ -10,18 +10,22 @@ fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
+        <main class="flex flex-col justify-center items-center h-screen bg-slate-200">
         <div>
-            <button
-                on:click=move |_| set_count.set(count.get() + 1)
-            >
-                "+"
-            </button>
-            {count}
-            <button
-                on:click=move |_| set_count.set(count.get() - 1)
-            >
-                "-"
-            </button>
-        </div>
+        <button
+            class="p-4 m-4 bg-white rounded-xl"
+            on:click=move |_| set_count.set(count.get() + 1)
+        >
+            "+"
+        </button>
+        {count}
+        <button
+            class="p-4 m-4 bg-white rounded-xl"
+            on:click=move |_| set_count.set(count.get() - 1)
+        >
+            "-"
+        </button>
+    </div>
+        </main>
     }
 }
