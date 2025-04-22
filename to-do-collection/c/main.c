4a90bd21c321fee9e420bb1db6363258651508f2
Mike <m.shevelinski@gmail.com>
2025-04-22 22:34:25 +0200

    Add is done selection

--- to-do-collection/c/main.c
+++ to-do-collection/c/main.c
@@ -3,6 +3,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 typedef struct ToDoItem {
     char *content;
     int is_selected;
@@ -13,7 +17,7 @@ void print_list(item items[]);
 
 int main() {
     item items[] = {
-        {"Hello, World", 0, 0},
+        {"Hello, World", 1, 0},
         {"Task 1", 0, 0},
         {"Task 2", 0, 0},
         {"Task 3", 0, 0},
@@ -29,9 +33,10 @@ int main() {
 
     while (1) {
         print_list(items);
-        switch (getchar()) {
+        int key = getchar();
+        switch (key) {
             case 65:
-                if (curr - 1 > 0) {
+                if (curr - 1 >= 0) {
                     items[curr].is_selected = 0;
                     curr = curr - 1;
                     items[curr].is_selected = 1;
@@ -44,7 +49,10 @@ int main() {
                     items[curr].is_selected = 1;
                 }
                 break;
+            case 10:
+            items[curr].is_done = !(items[curr].is_done);
             default:
+                // printf("key=%d", key);
                 break;
         }
     }
@@ -56,6 +64,18 @@ int main() {
 void print_list(item items[]) {
     printf("\e[1;1H\e[2J");
     for (int i = 0; i < 4; i++) {
-        printf("[%d] %s\n", items[i].is_selected, items[i].content);
+        char doneness;
+
+        if (items[i].is_done) {
+            doneness = '*';
+        } else {
+            doneness = ' ';
+        }
+
+        if (items[i].is_selected) {
+            printf(ANSI_COLOR_RED "[%c] %s" ANSI_COLOR_RESET "\n", doneness, items[i].content);
+        } else {
+            printf("[%c] %s\n", doneness, items[i].content);
+        }
     }
 }
