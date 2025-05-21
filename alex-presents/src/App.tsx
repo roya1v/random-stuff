2185217f16e911a7fac8152c018b8f7c387162d8
Mike <m.shevelinski@gmail.com>
2025-05-21 22:33:00 +0200

    Add simple list of presents

--- alex-presents/src/App.tsx
+++ alex-presents/src/App.tsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import { collection, doc, getDocs, onSnapshot, query } from 'firebase/firestore';
+import { useEffect, useState } from 'react'
+import { db } from './firebase';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [presents, setPresents] = useState<string[]>([])
+
+  useEffect(() => {
+    // const unsub = onSnapshot(doc(db, "presents"), (doc) => {
+    //   console.log("Current data: ", doc.data());
+    // });
+    getDocs(query(collection(db, "presents"))).then((docs) => {
+      const items: string[] = []
+      docs.forEach((doc) => { items.push(doc.data()["title"]) })
+      setPresents(items)
+    })
+  });
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">    Hello world!  </h1>
+      <h1 className="text-3xl font-bold underline">Подарки для Александра!</h1>
+      {presents.map((present) => { return <>{present}</> })}
     </>
   )
 }
---
+++ alex-presents/src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC4LkXuL3F68JRw4Xf0fgfCznxe7MMH_Xc",
+    authDomain: "alexpresents-91fa4.firebaseapp.com",
+    projectId: "alexpresents-91fa4",
+    storageBucket: "alexpresents-91fa4.firebasestorage.app",
+    messagingSenderId: "587656771100",
+    appId: "1:587656771100:web:4da39606bc0f10f678d1d9",
+    measurementId: "G-DNKXE887ZP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
