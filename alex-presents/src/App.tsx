15fae85a48f682463e11d44ed1331c2dafd00628
Mike <m.shevelinski@gmail.com>
2025-05-21 23:05:46 +0200

    Add taking of things

--- alex-presents/src/App.tsx
+++ alex-presents/src/App.tsx
@@ -1,6 +1,7 @@
-import { collection, doc, getDocs, onSnapshot, query, QueryDocumentSnapshot, type DocumentData } from 'firebase/firestore';
+import { collection, doc, getDocs, onSnapshot, query, QueryDocumentSnapshot, setDoc, updateDoc, type DocumentData } from 'firebase/firestore';
 import { useEffect, useState } from 'react'
 import { db } from './firebase';
+import { Button } from './components/ui/button';
 
 function App() {
   const [presents, setPresents] = useState<QueryDocumentSnapshot<DocumentData, DocumentData>[]>([])
@@ -18,9 +19,11 @@ function App() {
       <h1 className="text-3xl font-bold underline">Подарки для Александра!</h1>
       {presents.map((present) => {
         return <>
-          <div key={present.id}>
+          <div key={present.id} className="">
             {present.data()["title"]}
-
+            <Button onClick={() => {
+              updateDoc(doc(db, "presents", present.id), { taken: "test" })
+            }}>Выбрать</Button>
           </div>
         </>
       })}
