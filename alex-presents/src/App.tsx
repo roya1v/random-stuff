da2c2bc1ab5f7285761ba165c659c04d34b88087
Mike <m.shevelinski@gmail.com>
2025-05-23 19:38:22 +0200

    Add loading

--- alex-presents/src/App.tsx
+++ alex-presents/src/App.tsx
@@ -6,7 +6,7 @@ import { PresentsPage } from './pages/presentsPage/PresentsPage';
 
 function App() {
 
-    const [isLoggedIn, setLoggedIn] = useState(false);
+    const [isLoggedIn, setLoggedIn] = useState<boolean | null>(null);
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
@@ -18,18 +18,30 @@ function App() {
         });
     })
 
-    if (isLoggedIn) {
+    if (isLoggedIn == null) {
         return (
-            <>
-                <PresentsPage />
-            </>
+            <div className="flex justify-center items-center h-screen">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="animate-spin"
+                >
+                    <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+                </svg>
+            </div>
         )
+    } else if (isLoggedIn) {
+        return (<PresentsPage />)
+
     } else {
-        return (
-            <>
-                <AuthPage />
-            </>
-        )
+        return (<AuthPage />)
     }
 }
 
