986c1b70469cc0220e99d9852743d9dc0fee266a
Mike <m.shevelinski@gmail.com>
2025-07-08 21:31:41 +0200

    W in P

--- to-do-collection/react-native/app/items.tsx
+++ to-do-collection/react-native/app/items.tsx
@@ -1,19 +1,61 @@
 import * as schema from "@/db/schema";
 import { drizzle, useLiveQuery } from "drizzle-orm/expo-sqlite";
 import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
+import { useRouter } from "expo-router";
 import { useSQLiteContext } from "expo-sqlite";
 import { Appbar, Button, List } from "react-native-paper";
+
+function ItemsList() {
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db, { schema });
+  const { data } = useLiveQuery(drizzleDb.select().from(schema.todoItems));
+  useDrizzleStudio(db);
+  const router = useRouter();
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.Content title="Items" />
+        <Appbar.Action
+          icon="plus"
+          onPress={() => {
+            router.navigate("/newItem");
+          }}
+        />
+      </Appbar.Header>
+      {data.map((item) => (
+        <List.Item key={item.id} title={item.description}></List.Item>
+      ))}
+      <Button
+        mode="elevated"
+        onPress={async () => {
+          await drizzleDb
+            .insert(schema.todoItems)
+            .values({ description: "New item" });
+          console.log("Tapped!");
+        }}
+      >
+        Add new item
+      </Button>
+    </>
+  );
+}
 
 export default function Items() {
   const db = useSQLiteContext();
   const drizzleDb = drizzle(db, { schema });
   const { data } = useLiveQuery(drizzleDb.select().from(schema.todoItems));
   useDrizzleStudio(db);
+  const router = useRouter();
   return (
     <>
       <Appbar.Header>
         <Appbar.Content title="Items" />
-        <Appbar.Action icon="plus" onPress={() => {}} />
+        <Appbar.Action
+          icon="plus"
+          onPress={() => {
+            router.navigate("/newItem");
+          }}
+        />
       </Appbar.Header>
       {data.map((item) => (
         <List.Item key={item.id} title={item.description}></List.Item>
---
+++ to-do-collection/react-native/app/newItem.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native-paper";
+
+export default function NewItem() {
+  return <Text>Hello, World!</Text>;
+}
