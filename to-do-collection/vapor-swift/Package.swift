2f8967e36d89e4384aa4d1d6b4f87cfbfa46e8ac
Mike <m.shevelinski@gmail.com>
2025-07-21 21:02:36 +0200

    Add swift vapor version

---
+++ to-do-collection/vapor-swift/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "TodoServer",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "TodoServer",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "TemplateBareTests",
+            dependencies: [
+                .target(name: "TemplateBare"),
+                .product(name: "VaporTesting", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ExistentialAny"),
+] }
