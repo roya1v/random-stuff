dc3f1a2a52e641e8ba913b4475fb365ed6a1197e
Mike <m.shevelinski@gmail.com>
2025-09-24 22:58:42 +0200

    Clear field on submit

--- to-do-collection/react-client/src/NewTodoField.jsx
+++ to-do-collection/react-client/src/NewTodoField.jsx
@@ -6,8 +6,11 @@ export function NewTodoField() {
     const create = useTodoStore((state) => state.create)
     return (
       <div>
-        <input type="text" onChange={(e) => setContent(e.target.value)}></input>
-        <button onClick={() => create(content)}>Create</button>
+        <input type="text" onChange={(e) => setContent(e.target.value)} value={content}></input>
+        <button onClick={async () => {
+          await create(content)
+          setContent('')
+        }}>Create</button>
       </div>
     )
   }
